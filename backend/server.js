b675c8f4f0c9755bb01740579820e7a1951b2690
Morin <morin.alexandre1@gmail.com>
2021-08-30 17:02:28 +0200

    config sequelize

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,18 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+  
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome Alex" });
 });
+
+require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
